2a05e9fb03ce9f336108a062d92ec6a9ab5cac12
Well Paid Geek <wellpaidgeek@gmail.com>
2018-08-19 17:09:53 +0100

    First commit

---
+++ components/navigation/navigation.container.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
+import Navigation from './navigation';
+
+const pages = [
+    { path: '/', title: 'Search' },
+    { path: '/trending', title: 'Trending' },
+    { path: '/random', title: 'Random' },
+];
+
+function mapStateToProps () {
+    return { pages };
+}
+
+function mapDispatchToProps (dispatch) {
+    return {
+        navigate: (path) => dispatch(push(path)),
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
